2acc85f46367740cc21e9ba58e464433338c3446
Leonardo Egito <leoegitoadm@gmail.com>
2020-08-30 09:32:34 -0300

    Categorias de produtos

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -28,7 +28,7 @@ Class Category extends Model{
 
 		$this->setData($results[0]);
 
-		var_dump($results);
+		Category::updateFile();
 
 	}
 
@@ -52,6 +52,21 @@ Class Category extends Model{
 			"idcategory"=>$this->getidcategory()
 		));
 
+		Category::updateFile();
+
+	}
+
+	public static function updateFile(){
+
+		$categories = Category::listAll();
+
+		$html = [];
+
+		foreach ($categories as $row) {
+			array_push($html, '<li><a href="/categories/' .$row['idcategory'] .'">' .$row['descategory'] .'</a></li>');
+		}
+
+		file_put_contents($_SERVER['DOCUMENT_ROOT'] .DIRECTORY_SEPARATOR . "views" .DIRECTORY_SEPARATOR ."categories-menu.html", implode('', $html));
 	}
 
 }
