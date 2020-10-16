510e56cc918966362e5f4732b0dcd8be0677881a
Leonardo Egito <leoegitoadm@gmail.com>
2020-10-16 00:11:36 -0300

    Carrinho de compras - Primeira parte

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -12,6 +12,48 @@ Class User extends Model{
 	const SECRET = "HcodePhp7_Secret";
 	const SECRET_IV = "HcodePhp7_Secret_IV";
 
+	public static function getFromSession(){
+
+		$user = new User();
+
+		if(isset($_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION] > 0){
+
+			$user->setData($_SESSION[User::SESSION]);
+
+		}
+
+		return $user;
+
+	}
+
+	public static function checkLogin($inadmin = true){
+
+		if(!isset($_SESSION[User::SESSION]) 
+			|| !$_SESSION[User::SESSION] 
+			|| !(int)$_SESSION[User::SESSION]["iduser"] > 0 
+		)
+		{
+			//Não está logado
+
+		} else{
+
+			if($inadmin === true && (bool)$_SESSION[User::SESSION]["inadmin"] === true){
+
+				return true;
+
+			} else if ($inadmin === false){
+
+				return true;
+
+			} else{
+
+				return false;
+
+			}
+		}
+
+	}
+
 	public static function login($login, $password){
 
 		$sql = new Sql();
@@ -45,8 +87,9 @@ Class User extends Model{
 		if(
 			!isset($_SESSION[User::SESSION]) 
 			|| !$_SESSION[User::SESSION] 
-			|| !(int)$_SESSION[User::SESSION]["iduser"] > 0 
-			|| (bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
+			|| !(int)$_SESSION[User::SESSION]["iduser"] > 0
+			||
+			(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
 		){
 
 			header("Location: /admin/login");
